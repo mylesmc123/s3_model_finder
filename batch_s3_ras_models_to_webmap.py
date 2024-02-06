136b2e927fec2b2cad343857a0e92e68b123ba91
Myles McManus <mmcmanus@thewaterinstitute.org>
2024-02-06 16:09:53 -0600

    combined geoJsons to a single feature layer

--- batch_s3_ras_models_to_webmap.py
+++ batch_s3_ras_models_to_webmap.py
@@ -234,5 +234,35 @@ for region in last_plan_files:
 output_layers_list.output_layers_list('./output/perimeter/', './output/perimeter_list.json')
 
 # %%
+# for each geojson in ./output/perimeter, combine them into a single geojson file as a single featureCollection.
+geojsons = glob.glob('./output/perimeter/*.geojson')
+print('geojsons:', geojsons)
+# %%
+# create a featureCollection
+featureCollection = {
+    'type': 'FeatureCollection',
+    'features': []
+}
+# %%
+# for each geojson, open and append to featureCollection
+for geojson in geojsons:
+    with open(geojson) as f:
+        data = json.load(f)
+        featureCollection['features'].append(data['features'][0])
+
+#%%
+# get todays date
+from datetime import date
+today = date.today()
+# ddMMMYYYY
+today = today.strftime("%d%b%Y")
+print("Today's date:", today)
+        
+# %%
+# write featureCollection to file
+with open(f'./output/perimeters_combined_{today}.geojson', 'w') as f:
+    json.dump(featureCollection, f)
+
+# %%
 print("-------Done.-------")
 # %%
